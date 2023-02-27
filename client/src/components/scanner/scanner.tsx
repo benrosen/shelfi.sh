8290b473fd54fd214b60118e91bf9051b85d0094
Ben Rosen <benjamin.p.rosen@gmail.com>
2023-02-27 18:50:19 -0500

    add webcam

--- client/src/components/scanner/scanner.tsx
+++ client/src/components/scanner/scanner.tsx
@@ -1,22 +1,82 @@
 import { PanoramaFishEye } from "@mui/icons-material";
-import { Box, Button, Paper, Stack } from "@mui/material";
+import {
+  Box,
+  Button,
+  CircularProgress,
+  Grow,
+  Paper,
+  Stack,
+} from "@mui/material";
+import { useCallback, useRef, useState } from "react";
 import { Link } from "react-router-dom";
+import Webcam from "react-webcam";
 import { HelpSwitch } from "../help-switch";
 import { NavigationBar } from "../navigation-bar";
 import { RoutePaths } from "../router";
 
 export const Scanner = () => {
+  const webcamReference = useRef<Webcam>(null);
+
+  const [image, setImage] = useState<string | null>(null);
+
+  const [hasUserMedia, setHasUserMedia] = useState<boolean>();
+
+  const captureImage = useCallback(() => {
+    if (!webcamReference?.current) {
+      return;
+    }
+
+    setImage(webcamReference.current.getScreenshot());
+  }, [webcamReference]);
+
   return (
     <Stack direction="column" spacing={2}>
       <NavigationBar />
-      <Paper variant="outlined" sx={{ overflow: "hidden", borderRadius: 2 }}>
-        <Box height={320} p={1} />
-      </Paper>
+      {!hasUserMedia && (
+        <Stack direction="column" alignItems="center">
+          <CircularProgress />
+        </Stack>
+      )}
+      <Grow in={hasUserMedia}>
+        <Paper
+          elevation={4}
+          sx={{
+            overflow: "hidden",
+            borderRadius: 2,
+          }}
+        >
+          <Webcam
+            autoPlay
+            ref={webcamReference}
+            style={{
+              display: hasUserMedia ? "block" : "none",
+              width: "100%",
+            }}
+            onUserMedia={() => {
+              // TODO refactor this hacky timeout
+              // mitigate video loading duration variance
+              setTimeout(() => setHasUserMedia(true), 1500);
+            }}
+            onUserMediaError={() => {
+              setHasUserMedia(false);
+            }}
+          />
+        </Paper>
+      </Grow>
       <Stack direction="row" justifyContent="space-between" alignItems="center">
-        <Box sx={{ visibility: "hidden" }}>
+        <Box
+          sx={{
+            visibility: "hidden",
+          }}
+        >
           <HelpSwitch />
         </Box>
-        <Button variant="contained" component={Link} to={RoutePaths.Editor}>
+        <Button
+          variant="contained"
+          component={Link}
+          to={RoutePaths.Editor}
+          disabled={!hasUserMedia}
+        >
           <PanoramaFishEye />
         </Button>
         <HelpSwitch />
