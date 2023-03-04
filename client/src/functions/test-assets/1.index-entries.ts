926e25b5c655a57a02b7020cf4312cdaf9506b33
Ben Rosen <benjamin.p.rosen@gmail.com>
2023-03-04 08:07:10 -0500

    create test assets

---
+++ client/src/functions/test-assets/1.index-entries.ts
@@ -0,0 +1,196 @@
+import { IndexEntry } from "../../types";
+
+export default [
+  {
+    label: "abstract class",
+    pages: ["15", "16c", "359", "364", "365C"],
+  },
+  {
+    label: "abstract coupling",
+    pages: ["see coupling, abstract"],
+  },
+  {
+    label: "abstract coupling: in OBSERVER",
+    pages: ["296"],
+  },
+  {
+    label: "ABSTRACT FACTORY",
+    pages: ["87"],
+  },
+  {
+    label: "ABSTRACT FACTORY: extensibility of",
+    pages: ["91"],
+  },
+  {
+    label: "ABSTRACT FACTORY: in catalog summary",
+    pages: ["8"],
+  },
+  {
+    label: "ABSTRACT FACTORY: Lexi's use of",
+    pages: ["51"],
+  },
+  {
+    label: "ABSTRACT FACTORY: used to configure a subsystem",
+    pages: ["193"],
+  },
+  {
+    label: "Abstract Factory: participant of ABSTRACT FACTORY",
+    pages: ["88c", "89"],
+  },
+  {
+    label: "abstract operation",
+    pages: ["see operation, abstract"],
+  },
+  {
+    label: "abstract operation: use to implement ADAPTER",
+    pages: ["144"],
+  },
+  {
+    label: "abstract syntax tree",
+    pages: ["244", "251", "331"],
+  },
+  {
+    label: "abstract syntax tree: class structure for",
+    pages: ["244c", "331c"],
+  },
+  {
+    label: "abstract syntax tree: constructing in Smalltalk",
+    pages: ["250"],
+  },
+  {
+    label: "abstract syntax tree: object structure for",
+    pages: ["244o"],
+  },
+  {
+    label: "AbstractExpression: participant of INTERPRETER",
+    pages: ["245c", "245"],
+  },
+  {
+    label: "Abstraction: participant of BRIDGE",
+    pages: ["153c", "154"],
+  },
+  {
+    label: "AbstractProduct: participant of ABSTRACT FACTORY",
+    pages: ["88c", "89"],
+  },
+  {
+    label: "accumulating state",
+    pages: ["336"],
+  },
+  {
+    label: "acquaintance",
+    pages: ["22", "359"],
+  },
+  {
+    label: "acquaintance: C++, defined in",
+    pages: ["23"],
+  },
+  {
+    label: "acquaintance: compared with aggregation",
+    pages: ["23"],
+  },
+  {
+    label: "acquaintance: Smalltalk, defined in",
+    pages: ["23"],
+  },
+  {
+    label: "Action",
+    pages: ["see COMMAND"],
+  },
+  {
+    label: "active iterator",
+    pages: ["see iterator, active"],
+  },
+  {
+    label: "Ada",
+    pages: ["4, 21"],
+  },
+  {
+    label: "Adaptee: participant of ADAPTER",
+    pages: ["141", "141c"],
+  },
+  {
+    label: "ADAPTER",
+    pages: ["139"],
+  },
+  {
+    label: "ADAPTER: compared with BRIDGE",
+    pages: ["161", "219"],
+  },
+  {
+    label: "ADAPTER: compared with DECORATOR",
+    pages: ["184"],
+  },
+  {
+    label: "ADAPTER: compared with PROXY",
+    pages: ["216"],
+  },
+  {
+    label: "ADAPTER: in catalog summary",
+    pages: ["8"],
+  },
+  {
+    label: "Adapter: participant of ADAPTER",
+    pages: ["141", "141c"],
+  },
+  {
+    label: "adapter",
+    pages: ["140"],
+  },
+  {
+    label: "adapter: class adapter",
+    pages: ["141", "141c"],
+  },
+  {
+    label: "adapter: object adapter",
+    pages: ["141", "141c"],
+  },
+  {
+    label: "adapter: parameterized",
+    pages: ["145"],
+  },
+  {
+    label: "adapter: pluggable",
+    pages: ["see pluggable adapter"],
+  },
+  {
+    label: "adapter: two way",
+    pages: ["143", "143c"],
+  },
+  {
+    label: "adorner",
+    pages: ["179"],
+  },
+  {
+    label: "Aggregate: participant of ITERATOR",
+    pages: ["259", "259c"],
+  },
+  {
+    label: "aggregate object",
+    pages: ["257"],
+  },
+  {
+    label: "aggregation",
+    pages: ["22", "359"],
+  },
+  {
+    label: "aggregation: C++, defined in",
+    pages: ["23"],
+  },
+  {
+    label: "aggregation: compared with acquaintance",
+    pages: ["23"],
+  },
+  {
+    label: "aggregation: notation for",
+    pages: ["23"],
+  },
+  {
+    label: "aggregation: Smalltalk, defined in",
+    pages: ["23"],
+  },
+  {
+    label: "Alexander, Christopher",
+    pages: ["xiii", "2", "356", "358"],
+  },
+] as IndexEntry[];
